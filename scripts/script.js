7ea7638c7fa3f8330f06fb82deea7265081e66bd
SnowCoder404 <lenschaumarco@protonmail.com>
2024-09-10 00:38:49 +0200

    Header and burger menu is finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
     document.getElementById("myLinks").classList.toggle("d_none")
-    document.getElementById("menu").classList.toggle("menu")
+    document.getElementById("menu").classList.toggle("d_none")
 }
