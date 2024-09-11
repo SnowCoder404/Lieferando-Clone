84daf2ae0479ce8aa31d993ddb9d737cde8954bd
SnowCoder404 <lenschaumarco@protonmail.com>
2024-09-11 22:28:48 +0200

    change int to float

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,6 @@ function loadTemplate() {
 function addMealToShppingCart(index) {
     let totalPrice = document.getElementById("totalPrice");
     let singlePrice = myDishes[index].price;
-    
     document.getElementById("shoppingCart").classList.remove("d_none");
     if (totalPrice.innerText != "") {
         let totalPriceInt = singlePrice + parseFloat(totalPrice.innerText, 10);  
@@ -22,23 +21,6 @@ function addMealToShppingCart(index) {
     }else {
         totalPrice.innerText = singlePrice + " €";
     }
-    if (mealArray.length > 0) {
-        for (let i = 0; i < mealArray.length; i++) {
-            let meal = mealArray[i];
-            if (meal == myDishes[index].name) {
-    
-            }else {
-                mealArray.push(myDishes[index].name);
-                document.getElementById("mealList").innerHTML += addMeal(index);
-            }    
-        }
-    }else {
-        mealArray.push(myDishes[index].name);
-        document.getElementById("mealList").innerHTML += addMeal(index);
-    }
-  
-    
-    
- 
-
+    mealArray.push(myDishes[index].name);
+    document.getElementById("mealList").innerHTML += addMeal(index)    
 }
