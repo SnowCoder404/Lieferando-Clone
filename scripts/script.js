4c67ac84f189e2550dc9fbf71339c8129416f7b4
SnowCoder404 <lenschaumarco@protonmail.com>
2024-09-11 22:12:20 +0200

    Add multiple products

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
+let mealArray = [];
+
 function toggleMenu() {
     document.getElementById("shoppingCart").classList.toggle("d_none")
-    // document.getElementById("menu").classList.toggle("d_none")
 }
 
 function loadTemplate() {
     content = document.getElementById("mainContent");
-
     for (let index = 0; index < myDishes.length; index++) {
         content.innerHTML += templateHTML(index);
     }
@@ -14,12 +14,31 @@ function loadTemplate() {
 function addMealToShppingCart(index) {
     let totalPrice = document.getElementById("totalPrice");
     let singlePrice = myDishes[index].price;
+    
     document.getElementById("shoppingCart").classList.remove("d_none");
     if (totalPrice.innerText != "") {
-        let totalPriceInt = singlePrice + parseInt(totalPrice.innerText, 10);  
+        let totalPriceInt = singlePrice + parseFloat(totalPrice.innerText, 10);  
         totalPrice.innerText = totalPriceInt.toFixed(2) + " €";
     }else {
         totalPrice.innerText = singlePrice + " €";
     }
-    document.getElementById("mealList").innerHTML += addMeal(index);
+    if (mealArray.length > 0) {
+        for (let i = 0; i < mealArray.length; i++) {
+            let meal = mealArray[i];
+            if (meal == myDishes[index].name) {
+    
+            }else {
+                mealArray.push(myDishes[index].name);
+                document.getElementById("mealList").innerHTML += addMeal(index);
+            }    
+        }
+    }else {
+        mealArray.push(myDishes[index].name);
+        document.getElementById("mealList").innerHTML += addMeal(index);
+    }
+  
+    
+    
+ 
+
 }
